e7654d0620be540b5befb3e4c0612f8734d0182a
Swornim Barahi <39847629+swornimbarahi@users.noreply.github.com>
2019-07-10 22:20:55 -0700

    Created input block component and added story for it (#4)
    * Created input block component and added story for it
    
    * Changed validation as a prop and the way regex is handled
    
    * Removed unused declarations
    
    * Fix: Validation message showing before blur
    
    * Changed unnecessary interface to type

---
+++ src/components/Inputs/InputBlock/InputBlock.stories.tsx
@@ -0,0 +1,47 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import InputBlock, { ValidationMessageType } from './InputBlock';
+import '../../../styles/components/Inputs/InputBlock.scss';
+
+const backgroundStyle = {
+	width: '100%',
+	height: '70vh',
+	backgroundColor: 'black'
+};
+
+const inputBlockContainer = {
+	width: '300px',
+	padding: '50px'
+};
+
+const vMsg: ValidationMessageType = {
+	message: 'Please enter a valid email',
+	regex: /.+@.+\..+/
+};
+
+storiesOf('InputBlock', module)
+	.add('without regex', () => (
+		<div style={backgroundStyle}>
+			<div style={inputBlockContainer}>
+				<InputBlock
+					type="text"
+					onChange={(val: string) => console.log(val)}
+					label="Email"
+					placeholder="Insert Email Here"
+				/>
+			</div>
+		</div>
+	))
+	.add('with regex', () => (
+		<div style={backgroundStyle}>
+			<div style={inputBlockContainer}>
+				<InputBlock
+					type="text"
+					onChange={(val: string) => console.log(val)}
+					label="Email"
+					placeholder="Insert Email Here"
+					validationMessage={vMsg}
+				/>
+			</div>
+		</div>
+	));
---
+++ src/components/Inputs/InputBlock/InputBlock.tsx
@@ -0,0 +1,83 @@
+import React, {
+	FunctionComponent,
+	useState,
+	ChangeEvent,
+	FocusEvent
+} from 'react';
+
+export type ValidationMessageType = {
+	message: string;
+	regex: RegExp;
+};
+
+export type InputBlockType = {
+	label?: string;
+	id?: string;
+	type: string;
+	className?: string;
+	placeholder?: string;
+	disabled?: boolean;
+	maxLength?: number;
+	onChange?: Function;
+	validationMessage?: ValidationMessageType;
+};
+
+const InputBlock: FunctionComponent<InputBlockType> = ({
+	label,
+	id,
+	type,
+	className,
+	placeholder,
+	disabled,
+	onChange,
+	maxLength,
+	validationMessage
+}) => {
+	const [attr, setAttr] = useState({
+		value: '',
+		hasChanged: false,
+		wasBlurred: false
+	});
+
+	const displayMsg = validationMessage
+		? attr.value.match(validationMessage.regex)
+			? false
+			: true
+		: true;
+
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setAttr({ ...attr, value: e.target.value, hasChanged: true });
+		if (onChange) onChange(e.target.value);
+	};
+
+	const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
+		e.preventDefault();
+		setAttr({ ...attr, wasBlurred: true });
+	};
+
+	return (
+		<div className={`ui-input-block-wrapper ${className}`}>
+			<label htmlFor={id} className="ui-input-block-label">
+				{label}
+			</label>
+			<input
+				type={type}
+				maxLength={maxLength}
+				onChange={e => handleChange(e)}
+				placeholder={placeholder}
+				disabled={disabled ? disabled : false}
+				onBlur={e => handleBlur(e)}
+				className="ui-input-block-field"
+			/>
+			<div className="ui-input-block-vmessage">
+				{displayMsg && attr.hasChanged && attr.wasBlurred
+					? validationMessage
+						? validationMessage.message
+						: ''
+					: ''}
+			</div>
+		</div>
+	);
+};
+
+export default InputBlock;
